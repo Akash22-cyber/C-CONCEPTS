9dbcfeb3953d0fe514fe93c655b7fbe9ae008427
Akash22-cyber <nathakana03@gmail.com>
2025-02-18 19:52:38 +0530

    Add files via upload

---
+++ break_continue_keyword.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main (){
+    for(int i=0;i<5;i++){
+        cout<<"Akash"<<endl;
+        break;
+        /* whenever break key word  you 
+         get in the loop it terminate that loop*/
+    }
+    cout<<"jadhav"<<endl;
+     
+     for (int i = 0; i <6; i++)
+     {
+        continue;
+        cout<<"love";
+        /* continue skips the code written 
+        bellow the continue word  */
+     }
+     cout<<"babbar";
+
+}
