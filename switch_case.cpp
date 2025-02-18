4165e20ca9f454babdad4dda27e6ce4791c1a962
Akash22-cyber <nathakana03@gmail.com>
2025-02-18 19:55:23 +0530

    Add files via upload

---
+++ switch_case.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    int val;
+    cin>>val;
+    switch(val){
+        case 1: cout<<"Akash";
+        break;
+        case 2:cout<<"Sarjerao";
+        break;
+        case 3:cout<<"jadhav";
+        break;
+        default: cout<<"Sharda";
+
+    }
+}
